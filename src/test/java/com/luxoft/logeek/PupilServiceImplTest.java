c57b0da038343135e0a834c566c204225ee06bf5
stsypanov <sergei.tsypanov@yandex.ru>
2018-06-06 21:50:30 +0300

    refactor

--- src/test/java/com/luxoft/logeek/PupilServiceImplTest.java
+++ src/test/java/com/luxoft/logeek/PupilServiceImplTest.java
@@ -1,8 +1,9 @@
 package com.luxoft.logeek;
 
 import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.luxoft.logeek.TestBase;
+import com.luxoft.logeek.entity.Ученик;
 import com.luxoft.logeek.service.PupilService;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Commit;
@@ -16,21 +17,28 @@ public class PupilServiceImplTest extends TestBase {
     @Autowired
     private PupilService pupilService;
 
+    @Before
+    public void setUp() {
+        pupilRepository.findById(1L)
+                .map(Ученик::названиеШколы)
+                .ifPresent(this::вывестиШколу);
+    }
+
     @Test
     public void changeSchool() {
-        pupilRepository
-                .findById(1L)
-                .ifPresent(ученик -> System.out.println("Моя старая школа: " + ученик.названиеШколы()));
         pupilService.сменитьШколу(1L, "ООШ №3");
     }
 
     @AfterTransaction
     public void tearDown() {
-        pupilRepository
-                .findById(1L)
-                .ifPresent(ученик -> {
-                    assertEquals("ООШ №3", ученик.названиеШколы());
-                    System.out.println("Моя новая школа: " + ученик.названиеШколы());
+        pupilRepository.findById(1L).map(Ученик::названиеШколы)
+                .ifPresent(школа -> {
+                    assertEquals("ООШ №3", школа);
+                    вывестиШколу(школа);
                 });
+    }
+
+    private void вывестиШколу(String школа) {
+        System.out.println("Моя школа: " + школа);
     }
 }
