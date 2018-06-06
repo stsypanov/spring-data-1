070393bc06e670d446740392df50747615b8d28d
stsypanov <sergei.tsypanov@yandex.ru>
2018-06-06 21:51:10 +0300

    улучшенный BaseJpaRepository::save

--- src/main/java/com/luxoft/logeek/repository/BaseJpaRepositoryImpl.java
+++ src/main/java/com/luxoft/logeek/repository/BaseJpaRepositoryImpl.java
@@ -2,18 +2,32 @@ package com.luxoft.logeek.repository;
 
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.io.Serializable;
 
 public class BaseJpaRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements BaseJpaRepository<T, ID> {
 
-    public BaseJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInfo, EntityManager entityManager) {
-        super(entityInfo, entityManager);
+    private final JpaEntityInformation<T, ?> entityInformation;
+    private final EntityManager entityManager;
+
+    public BaseJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+        this.entityInformation = entityInformation;
+        this.entityManager = entityManager;
     }
 
     @Override
+    @Transactional
     public <S extends T> S save(S entity) {
-        return super.save(entity);
+        if (entityInformation.isNew(entity)) {
+            entityManager.persist(entity);
+            return entity;
+        } else if (!entityManager.contains(entity)) {
+            return entityManager.merge(entity);
+        }
+
+        return entity;
     }
 }
