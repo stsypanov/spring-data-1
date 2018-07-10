93e8b6879082c69137e32af367cb33bc0ef3dd97
stsypanov <sergei.tsypanov@yandex.ru>
2018-07-10 22:33:15 +0300

    более точная настройка

--- src/main/java/com/luxoft/logeek/AppConfig.java
+++ src/main/java/com/luxoft/logeek/AppConfig.java
@@ -2,9 +2,14 @@ package com.luxoft.logeek;
 
 import com.luxoft.logeek.repository.BaseJpaRepositoryImpl;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(repositoryBaseClass = BaseJpaRepositoryImpl.class)
+@EnableJpaRepositories(
+        repositoryBaseClass = BaseJpaRepositoryImpl.class,
+        basePackages = "com.luxoft.logeek.repository"
+)
+@EntityScan(basePackages = "com.luxoft.logeek.entity")
 public class AppConfig {
 }
